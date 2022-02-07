0cb0d20e7cae8e4a319754131a69729c55c3b159
fedeceola <federico.ceola@iit.it>
2022-02-07 14:46:28 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='icub_mujoco',
     version='0.1.0',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+    	'dm_control==0.0.403778684',
+    	'stable-baselines3[extra]',
+    	'pyyaml',
+    	]
 )
