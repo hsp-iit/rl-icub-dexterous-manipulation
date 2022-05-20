229691974c576aafcb05ce405ae59668f446f288
fedeceola <federico.ceola@iit.it>
2022-05-20 10:11:13 +0200

    Added utils functions for pointcloud computation

---
+++ icub_mujoco/utils/pcd_utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def pcd_from_depth(depth):
+    pcd = np.empty(shape=(depth.size, 3))
+    pcd[:, 2] = np.reshape(depth, (depth.size,))
+    grid = np.mgrid[0:depth.shape[0], 0:depth.shape[1]]
+    u, v = np.reshape(grid[0], (grid[0].size, )), np.reshape(grid[1], (grid[1].size, ))
+    pcd[:, 0] = (u - 240) * pcd[:, 2] / 617.783447265625
+    pcd[:, 1] = (v - 320) * pcd[:, 2] / 617.783447265625
+    return pcd
+
+
+def points_in_world_coord(points, cam_xpos, cam_xmat):
+    xyzs = []
+    for point in points:
+        # Point roto-translation matrix in world coordinates
+        p_cam = np.array([[1, 0, 0, point[1]],
+                          [0, 1, 0, -point[0]],
+                          [0, 0, 1, point[2]],
+                          [0, 0, 0, 1]],
+                         dtype=np.float32)
+        cam_world = np.array([[1, 0, 0, 0],
+                              [0, 1, 0, 0],
+                              [0, 0, 1, 0],
+                              [0, 0, 0, 1]],
+                             dtype=np.float32)
+        cam_world[:3, -1] = cam_xpos
+        cam_world[:3, :3] = cam_xmat
+        # Point roto-translation matrix in camera coordinates
+        p_world = np.matmul(cam_world, p_cam)
+        xyzs.append(np.array([p_world[0, 3], p_world[1, 3], p_world[2, 3]]))
+    return np.array(xyzs)
