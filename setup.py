8dc965a059712814876a955f3000c0c48f8ce08b
fedeceola <federico.ceola@iit.it>
2022-09-02 14:17:40 +0200

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'imitation==0.3.1'
+        'imitation==0.3.1',
         'dm_control==0.0.403778684',
         'stable-baselines3[extra]==1.5.0',
         'pyyaml',
