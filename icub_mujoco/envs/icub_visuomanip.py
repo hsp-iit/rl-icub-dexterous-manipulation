32aea2e4788e94a062cecc527ad818d5d03d4dab
fedeceola <federico.ceola@iit.it>
2022-11-22 16:36:00 +0100

    Minor indentation change

--- icub_mujoco/envs/icub_visuomanip.py
+++ icub_mujoco/envs/icub_visuomanip.py
@@ -1256,11 +1256,11 @@ class ICubEnv(gym.Env):
                 if contact.geom1 in self.contact_geom_ids_fingers_meshes.values():
                     self.fingers_touching_object.append((list(
                         self.contact_geom_ids_fingers_meshes.keys())[list(
-                        self.contact_geom_ids_fingers_meshes.values()).index(contact.geom1)]))
+                            self.contact_geom_ids_fingers_meshes.values()).index(contact.geom1)]))
                 else:
                     self.fingers_touching_object.append((list(
                         self.contact_geom_ids_fingers_meshes.keys())[list(
-                        self.contact_geom_ids_fingers_meshes.values()).index(contact.geom2)]))
+                            self.contact_geom_ids_fingers_meshes.values()).index(contact.geom2)]))
         self.number_of_contacts = len(set(self.fingers_touching_object))
         return self.number_of_contacts
 
